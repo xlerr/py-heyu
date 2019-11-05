4681b39c69204f52eba2ef3e1f3a94c9405895b1
xlerr <xlerr@qq.com>
2019-11-06 02:02:28 +0800

    t

--- app.py
+++ app.py
@@ -1,34 +1,57 @@
 from PIL import Image, ImageDraw, ImageFont
 from PIL import ImageOps
+from configparser import ConfigParser
+import re
+import string
+import os
 
 
-def transparent_back(img):
-    img = img.convert('RGBA')
-    L, H = img.size
+def hex2rgb(hex_color: string):
+    hex_color = hex_color.strip('#')
+    opt = re.findall(r'(.{2})', hex_color)
+    color = []
+    for i in range(0, len(opt)):
+        color.append(int(opt[i], 16))
+    return tuple(color)
+
+
+def transparent_back(imgIns, auto_graph_color):
+    R, G, B = auto_graph_color
+    imgIns = imgIns.convert('RGBA')
+    L, H = imgIns.size
     for h in range(H):
         for l in range(L):
             dot = (l, h)
-            r, g, b, a = img.getpixel(dot)
+            r, g, b, a = imgIns.getpixel(dot)
             if r > 0 & r < 255:
                 a = r
             else:
                 a = 0
-            img.putpixel(dot, (255, 0, 0, a))
-    return img
+            imgIns.putpixel(dot, (R, G, B, a))
+    return imgIns
 
+
+cfg = ConfigParser()
+cfg.read('config.ini')
+
+text = cfg.get('title', 'text')
+text_color = cfg.get('title', 'color')
+
+autograph_color = hex2rgb(cfg.get('autograph', 'color'))
 
 img = Image.open('./static/source.png')
+img = img.resize((824, 488), Image.ANTIALIAS)
 
 box = (115, 100, 690, 360)
 img = img.crop(box)
 
 # img = ImageOps.invert(img)
 
-img = transparent_back(img)
+img = transparent_back(img, autograph_color)
 
 width, height = img.size
 
-nImg = Image.new("RGBA", (width * 4, height), (0, 0, 0, 0))
+nImg = Image.new("RGBA", (width * 4, height))
 
 for i in range(0, 4):
     pos = (width * i, 0)
@@ -38,14 +61,16 @@ width, height = (2409, int(2409 / nImg.size[0] * height))
 
 nImg = nImg.resize((width, height), Image.ANTIALIAS)
 
-img = Image.new("RGBA", (2479, 3508), (0, 0, 0, 0))
+img = Image.new("RGBA", (2479, 3508), (255, 255, 255, 255))
 for i in range(0, 11):
-    pos = (30, 3508 - (height + 30) * (i + 1))
-    img.paste(nImg, pos)
+    pos = (30, 3508 - (height + 30 * int(bool(i))) * (i + 1))
+    img.paste(nImg, pos, nImg)
 
-font = ImageFont.truetype('./static/simsun.ttc', 82, encoding="unic")
+font = ImageFont.truetype('./static/simsun.ttc', 82, index=1)
+
+text_width, text_height = font.getsize(text)
 
 draw = ImageDraw.Draw(img)
-draw.text((350, 40), "了卡就是的了疯狂骄傲爱上了的开发就", fill="red", font=font)
+draw.text((int((width - text_width) / 2), 40), text, fill=text_color, font=font)
 
 img.save('./temp.png')
